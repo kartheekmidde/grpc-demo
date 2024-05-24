4e31d58ec87d398afe51751473ce240e47b6d0c2
Kartheek Midde <kartheek.midde@clearlink.com>
2024-05-24 13:38:30 -0600

    Set up server configuration

--- server_node/cricket_server.js
+++ server_node/cricket_server.js
@@ -7,3 +7,21 @@ const grpcObject = grpc.loadPackageDefinition(packageDefinition)
 const cricketPackage = grpcObject.Cricket
 
 const server = new grpc.Server()
+
+server.addService(cricketPackage.service, {
+    GetMatchResult: (call, callback) => {},
+    GetLiveScore: (call) => {},
+    UpdatePlayerStats: (call, callback) => {},
+    Chat: (call) => {},
+})
+
+const address = '0.0.0.0:500051'
+
+server.bindAsync(address, grpc.ServerCredentials.createInsecure(), (err, port) => {
+    if (error) {
+        console.log(`Server error ${error}`)
+        return;
+    }
+    console.log(`Server running at ${address}`)
+    server.start()
+})
